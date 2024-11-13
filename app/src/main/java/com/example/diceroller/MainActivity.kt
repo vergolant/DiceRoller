6ef18261fd3e6ffc855b1fb02eb9ec50ed5ba965
Anatoly <vergolant@gmail.con>
2024-11-13 14:52:09 +0300

    Приложение готово, присутствует глюк, при котором не происходит повторый бросок

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.diceroller
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -17,6 +18,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.diceroller.ui.theme.DiceRollerTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,16 +49,37 @@ fun DiceRollerApp(){
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier
     .fillMaxSize()
     .wrapContentSize(Alignment.Center)
- ){
+){
+    var state = remember { mutableStateOf(1) }
+    val imageResource = when (state.value){
+        1-> R.drawable.dice_1
+        2-> R.drawable.dice_2
+        3-> R.drawable.dice_3
+        4-> R.drawable.dice_4
+        5-> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
-        Button(onClick = {/*TODO*/}){
+        Image(
+            painter = painterResource(imageResource),
+            contentDescription = state.toString()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {state.value = DrawClick}){
             Text(stringResource(R.string.roll))
         }
 
     }
 
 }
+var DrawClick = Draw()
+private fun Draw(): Int {
+    var result = (1..6).random()
+    return result
+}
+
+
 
